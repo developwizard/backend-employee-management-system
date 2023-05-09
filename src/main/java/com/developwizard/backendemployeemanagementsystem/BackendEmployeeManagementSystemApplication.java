1df3e8a6044aa4ad20f09920b5afc365a95620f6
developwizard <churkin.it@gmail.com>
2023-05-09 21:46:14 +0300

    Initial commit

---
+++ src/main/java/com/developwizard/backendemployeemanagementsystem/BackendEmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.developwizard.backendemployeemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEmployeeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEmployeeManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developwizard/backendemployeemanagementsystem/BackendEmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developwizard.backendemployeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
